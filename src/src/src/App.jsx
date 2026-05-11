1ac009b58e33d978b7810e63976595235e4f06f6
dvocsapacai01-eng <dvo_cs_apac_ai_01@callboxinc.com>
2026-05-11 09:51:37 +0800

    Delete src/src directory

--- src/src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Landing page is working</h1>
-      <p>React and Vercel are connected correctly.</p>
-    </main>
-  );
-}
